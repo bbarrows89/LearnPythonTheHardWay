54f863cffba43cef180cccd145b58f0a21b974e5
Bryan Barrows <bryanbarrows@live.com>
2016-06-19 11:25:19 -0700

    ex 4-5
    uploading completed examples 4 and 5

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Bryan Barrows'
+my_age = 26
+my_height = 71 # inches
+my_weight = 160 # lbs
+my_eyes = 'blue'
+my_teeth = 'white-ish'
+my_hair = 'salt and pepper'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "It's all muscle..."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+print "If I add %d, %d, and %d I get %d." % (
+	my_age, my_height, my_weight, my_age + my_height + my_weight)
